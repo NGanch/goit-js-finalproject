1ed80777a50f26515f481adb3637700405d8a4cd
NGanch <86801593+NGanch@users.noreply.github.com>
2023-10-25 17:50:06 +0100

    comit

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -6,7 +6,7 @@ function createImageCard(array) {
   const markup = array
     .map(({ preview, original, description }) => {
       return `<li class="gallery__item">
-        <a class="gallery__link" href="${preview}">
+        <a class="gallery__link" href="${original}">
            <img class="gallery__image"
             src="${original}"
              alt="${description}" />
